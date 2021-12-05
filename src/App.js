3a6a1e3129d8a4c3df1e7e843f254ad421693b66
Mad-016 <85418716+Mad-016@users.noreply.github.com>
2021-12-05 21:38:06 +1000

    Update style and link GitHub

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ export default function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Madeline Hardy</small>
+          <small>
+            Coded by Madeline Hardy. See my{" "}
+            <a href="https://github.com/Mad-016/dictionary-project">GitHub</a>.
+          </small>
         </footer>
       </div>{" "}
     </div>
