f6222a8851f1f6db341a108a3b1f0b93c2667243
Mad-016 <85418716+Mad-016@users.noreply.github.com>
2021-12-13 16:41:43 +1000

    Change h1

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
         </header>
 
         <main>
-          <Dictionary defaultKeyword="Fountain" />
+          <Dictionary defaultKeyword="Travel" />
         </main>
         <footer className="App-footer">
           <small>
